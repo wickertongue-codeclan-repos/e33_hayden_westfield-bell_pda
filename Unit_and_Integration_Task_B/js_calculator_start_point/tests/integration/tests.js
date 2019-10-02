7fc88af6fe13252b211f22170635aff306af3ea1
hayden <haydenwb@gmail.com>
2019-10-02 13:06:11 +0100

    started on integration tests

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -10,10 +10,34 @@ describe('calculator functionality', function() {
   });
 
   // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
+  // it('should have working number buttons', function(){
+  //   running_total = element(by.css('#running_total'))
+  //   element(by.css('#number2')).click();
+  //   expect(running_total.getAttribute('value')).to.eventually.equal('2');
+  // })
+
+  // it('the number buttons should update the display of the running total', function () {
+  //   running_total = element(by.css('#running_total'))
+  //   element(by.css('#number2')).click();
+  //   element(by.css('#number4')).click();
+  //   element(by.css('#number8')).click();
+  //   expect(running_total.getAttribute('value')).to.eventually.equal('248');
+  // })
+
+  it('the arithmetical operations update the display with the result of the operation', function () {
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
   })
 
 });
+
+
+
+
+// 3. Can multiple operations be chained together ?
+// 4. Is the output as expected for a range of numbers(for example, positive, negative, decimals and very large numbers)?
+// 5. What does the code do in exceptional circumstances ? Specifically, if you divide by zero, what is the effect ? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
